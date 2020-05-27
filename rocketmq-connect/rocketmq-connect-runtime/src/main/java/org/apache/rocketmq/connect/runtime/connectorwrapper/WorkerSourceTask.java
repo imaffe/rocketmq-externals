bf34045888a7536031099d0a01f01897e3e886e5
affe <affeisme@gmail.com>
2020-05-26 21:17:27 -0700

    WIP

--- rocketmq-connect/rocketmq-connect-runtime/src/main/java/org/apache/rocketmq/connect/runtime/connectorwrapper/WorkerSourceTask.java
+++ rocketmq-connect/rocketmq-connect-runtime/src/main/java/org/apache/rocketmq/connect/runtime/connectorwrapper/WorkerSourceTask.java
@@ -294,7 +294,7 @@ public class WorkerSourceTask implements WorkerTask {
         StringBuilder sb = new StringBuilder();
         sb.append("connectorName:" + connectorName)
             .append("\nConfigs:" + JSON.toJSONString(taskConfig))
-            .append("\nState:" + state.get().toString());;
+            .append("\nState:" + state.get().toString());
         return sb.toString();
     }
 }
